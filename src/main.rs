ebf9ec84acc03f39309b5b13d9e23ef918c0089d
Matilda Horger <campadrenalin@gmail.com>
2024-05-27 13:53:02 -0700

    Factor out parse.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-use markdown::{to_mdast, ParseOptions};
-use markdown::mdast::{Node,Code};
-use crate::segment::Segment;
-
+use crate::parse::parse;
 mod segment;
-
-// Recursively explore a Markdown parse tree and find all the code segments
-fn find_code_nodes(node: &Node, output_buf: &mut Vec<Code>) {
-    match node {
-        Node::Code(c) => output_buf.push(c.clone()),
-        _ => node.children().unwrap_or(&vec![]).iter().for_each(
-            |n| find_code_nodes(n, output_buf)
-        ),
-    };
-}
+mod parse;
 
 fn main() {
-    // Per docs: cannot fail with MDX off
     let md_text = "
 Hello, world!
 
@@ -24,13 +11,6 @@ Hello, world!
 const foo = 'bar';
 ```
     ";
-    let tree = to_mdast(md_text, &ParseOptions::default()).unwrap();
-    println!("Tree: {:?}", &tree);
-
-    let mut code_nodes: Vec<Code> = vec![];
-    find_code_nodes(&tree, &mut code_nodes);
-    println!("Code Nodes: {:?}", code_nodes);
-
-    let segments: Vec<Segment> = code_nodes.iter().map(|c| Segment::from(c)).collect();
+    let segments = parse(&md_text);
     println!("Segments: {:?}", segments);
 }
---
+++ src/parse.rs
@@ -0,0 +1,30 @@
+use markdown::{to_mdast, ParseOptions};
+use markdown::mdast::{Node,Code};
+use crate::segment::Segment;
+
+// This wraps up the higher-level process of generating segments from MD text.
+//
+// There's probably a lot of room to improve performance by eliminating copy
+// operations in memory - the sloppiness is because I'm still learning Rust.
+// I promise it itches me, but this will probably always be adequate.
+pub fn parse(md_text: &str) -> Vec<Segment> {
+    // Per docs: cannot fail with MDX off
+    let tree = to_mdast(md_text, &ParseOptions::default()).unwrap();
+
+    // Build a buffer of Code structs
+    let mut code_nodes: Vec<Code> = vec![];
+    find_code_nodes(&tree, &mut code_nodes);
+
+    // Convert them all!
+    return code_nodes.iter().map(|c| Segment::from(c)).collect();
+}
+
+// Recursively explore a Markdown parse tree and find all the code segments
+fn find_code_nodes(node: &Node, output_buf: &mut Vec<Code>) {
+    match node {
+        Node::Code(c) => output_buf.push(c.clone()),
+        _ => node.children().unwrap_or(&vec![]).iter().for_each(
+            |n| find_code_nodes(n, output_buf)
+        ),
+    };
+}
